ef4e3407b9140a0988d8dfb4c2e4f9673755ba5d
niemet0502 <vincentmarius8@gmail.com>
2022-04-28 00:36:11 +0100

    add update and delete as actions and in the reducer

--- src/store/taskActions.js
+++ src/store/taskActions.js
@@ -1,5 +1,9 @@
 export const ADD_TASK = "task/addTask";
 export const GET_TASKS = "task/getTasks";
+export const UPDATE_TASK = "task/updateTask";
+export const DELETE_TASK = "task/deleteTask";
 
 export const addTask = (task) => ({ type: ADD_TASK, payload: task });
+export const updateTask = (task) => ({ type: UPDATE_TASK, payload: task });
 export const getTask = () => ({ type: GET_TASKS });
+export const deleteTask = (task) => ({ type: DELETE_TASK, payload: task.id });
--- src/store/taskReducer.js
+++ src/store/taskReducer.js
@@ -6,11 +6,15 @@ const initialState = {
       id: 1,
       title: "plain redux 1",
       detail: "write the technical post on redux",
+      date: "",
+      isDueDate: false,
     },
     {
       id: 2,
       title: "plain redux 2",
       detail: "write the technical post on redux",
+      date: "",
+      isDueDate: false,
     },
   ],
 };
@@ -18,7 +22,30 @@ const initialState = {
 function taskReducer(state = initialState, action) {
   switch (action.type) {
     case actions.ADD_TASK:
-      return { ...state, tasks: [...state.tasks, action.payload] };
+      return {
+        ...state,
+        tasks: [
+          ...state.tasks,
+          { ...action.payload, id: state.tasks.length + 1 },
+        ],
+      };
+    case actions.UPDATE_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.map((task) => {
+          if (task.id === action.payload.id) {
+            return { ...task, ...action.payload };
+          } else {
+            return task;
+          }
+        }),
+      };
+
+    case actions.DELETE_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== action.payload),
+      };
     default:
       return state;
   }
