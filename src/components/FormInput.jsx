24f8b0894dc2132a040aa6958ffcdfe8c9477991
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-05-04 00:07:43 +0000

    refactoring the code by creating a FormInput component

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const FormInput = () => {
+const FormInput = ({value, handleChange, name, label, type}) => {
   return (
-    <div>FormInput</div>
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input type={type ? type : 'text'} name={name} value={value} onChange={handleChange} className="form-control mt-2" id={name} aria-describedby={`${label}Help`} />
+    </div>
   )
 }
 
